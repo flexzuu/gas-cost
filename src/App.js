81877d2c7b6844add2874533080625bd814f7c7a
Jonas Faber <dev.jonas.faber@gmail.com>
2016-09-07 02:05:10 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import logo from './logo.svg';
+import './App.css';
+import GasTable from './GasTable.js';
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      prices: {
+        autogas: 0,
+        diesel: 0,
+        erdgas: 0,
+        supere5: 0,
+        biodiesel: 0,
+        supere10: 0,
+        superplus: 0,
+        datum: null
+      },
+      lastUpdated: "Never...",
+      spin: true
+    }
+  }
+  componentDidMount() {
+    fetch('/kfz?markt=dut', {
+      method: 'get',
+      headers: new Headers({
+		      'Content-Type': 'text/plain'
+	   })
+    })
+    .then((res) => {
+        return res.json();
+    }).then((json) => {
+        this.setState({
+          prices: json,
+          lastUpdated: moment(parseInt(json.datum, 10)).fromNow(),
+          spin: false
+        });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className={this.state.spin ? "App-logo spin": "App-logo"} alt="logo" />
+          <h2>Globus Tankstelle Dutenhofen</h2>
+          <h3>Last updated: {this.state.lastUpdated}</h3>
+        </div>
+        <GasTable prices={this.state.prices} toHide={["datum", "biodiesel", "erdgas"]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/GasTable.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './GasTable.css';
+
+function Row({type, price}) {
+  return (
+    <tr>
+      <td className="title">{type.toUpperCase()}</td>
+      <td className="price">{price}€</td>
+    </tr>
+  );
+}
+
+class GasTable extends Component {
+  render() {
+    return (
+        <table className="gas">
+          <thead>
+            <tr><th className="title">TYPE</th><th className="price">PRICE</th></tr>
+          </thead>
+          <tbody>
+            {Object.keys(this.props.prices)
+              .filter((key)=> !this.props.toHide.includes(key))
+              .sort()
+              .map((key)=><Row key={key} type={key} price={this.props.prices[key]}/>)
+            }
+          </tbody>
+        </table>
+    );
+  }
+}
+
+export default GasTable;
