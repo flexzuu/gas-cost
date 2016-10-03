24928112b309285784a9f1a7ccfabce4bb20cb10
Jonas Faber <dev.jonas.faber@gmail.com>
2016-10-03 16:45:18 +0200

    Change gas to e5 and add display for reduced price.

--- src/GasTable.js
+++ src/GasTable.js
@@ -5,6 +5,7 @@ const GasPrice = ({label, price, color}) => (
   <tr>
     <td className="label" style={{color}}>{label}</td>
     <td className="price">{price}€</td>
+    <td className="price reduced">{Number((price-0.04).toFixed(3))}€</td>
   </tr>
 );
 
@@ -12,7 +13,7 @@ const GasTable = ({data}) => {
   return (
       <table className="gas">
         <thead>
-          <tr><th className="label">TYPE</th><th className="price">PRICE</th></tr>
+          <tr><th className="label">TYPE</th><th className="price">PRICE</th><th className="price reduced">PRICE REDUCED</th></tr>
         </thead>
         <tbody>
           {
